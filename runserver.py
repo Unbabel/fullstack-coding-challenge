3b0e7b57a1855b8ee3042acf9befd81899255d33
David Silva <david.silva24@hotmail.com>
2019-05-16 11:55:12 +0100

    Port changes

--- runserver.py
+++ runserver.py
@@ -2,13 +2,9 @@
 This script runs the UnbabelChallenge application using a development server.
 """
 
-from os import environ
+from import os
 from UnbabelChallenge import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
