00c32b10c6997460893df5e02bd03531738f8711
David Silva <david.silva24@hotmail.com>
2019-05-16 12:05:14 +0100

    More port experimentation with hosting.

--- runserver.py
+++ runserver.py
@@ -6,5 +6,4 @@ from import os
 from UnbabelChallenge import app
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run()
