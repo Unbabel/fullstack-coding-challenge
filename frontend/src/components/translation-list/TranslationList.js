4dcd332569484558ebd475597206acdca991f5cc
amosbastian <amosbastian@gmail.com>
2019-10-01 16:26:20 +0100

    Add basic status badge to translation

--- frontend/src/components/translation-list/TranslationList.js
+++ frontend/src/components/translation-list/TranslationList.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import Translation from '../translation/Translation';
 
+const statusMapper = {
+  new: 'badge-pending',
+  translating: 'badge-pending',
+  canceled: 'badge-danger',
+  failed: 'badge-danger',
+  rejected: 'badge-danger',
+  accepted: 'badge-success',
+  completed: 'badge-success',
+};
+
 const TranslationList = () => (
   <div className="flex flex-col bg-gray-100">
     <div className="bg-white px-4 py-3 border border-l-0 border-r-0 text-sm text-gray-600">
-      3 translations
+      7 translations
     </div>
-    <Translation status="new" />
-    <Translation status="translating" />
-    <Translation status="completed" />
-    <Translation status="failed" />
-    <Translation status="canceled" />
-    <Translation status="accepted" />
-    <Translation status="rejected" />
+    <Translation status="new" badgeClass={statusMapper.new} />
+    <Translation status="translating" badgeClass={statusMapper.translating} />
+    <Translation status="completed" badgeClass={statusMapper.completed} />
+    <Translation status="failed" badgeClass={statusMapper.failed} />
+    <Translation status="canceled" badgeClass={statusMapper.canceled} />
+    <Translation status="accepted" badgeClass={statusMapper.accepted} />
+    <Translation status="rejected" badgeClass={statusMapper.rejected} />
   </div>
 );
 
--- frontend/src/components/translation/Translation.js
+++ frontend/src/components/translation/Translation.js
@@ -4,9 +4,7 @@ const Translation = props => (
   <div className="px-4 py-3 flex flex-col bg-white border border-l-0 border-r-0 border-t-0">
     <div className="flex items-baseline justify-between">
       <span className="text-gray-500 text-sm">English &rarr; Spanish</span>
-      <span className="inline-block bg-teal-200 text-teal-800 text-xs px-2 rounded-full font-semibold">
-        {props.status}
-      </span>
+      <span className={`badge ${props.badgeClass}`}>{props.status}</span>
     </div>
     <div className="mt-2">
       <div className="text-gray-600">Original text...</div>
