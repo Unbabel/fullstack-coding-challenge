ea7fab8a390d3881ecb8fa41908251de3f762b53
amosbastian <amosbastian@gmail.com>
2019-10-01 13:15:20 +0100

    Add ESLint & Prettier

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import Header from '../header/Header';
+import LanguageSelector from '../language-bar/LanguageBar';
 
-const app = () => {
-  return(
-    <div className="App">
-      <Header></Header>
-      <header className="App-header">
-        <p>
-          Edit <code>src/app.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const app = () => (
+  <div className="App">
+    <Header></Header>
+    <LanguageSelector></LanguageSelector>
+  </div>
+);
 
 export default app;
--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
-const header = () => {
-    return(
-        <header className="bg-indigo-600 flex items-center justify-between px-4 py-3">
-            <div>
-                <button type="button" className="block text-gray-100">
-                    <svg className="h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
-                    </svg>
-                </button>
-            </div>
-            <div>
-                <img className="h-8" src="/" alt="Unbabel"></img>
-            </div>
-        </header>
-    );
-}
+const header = () => (
+  <header className="bg-indigo-600 flex items-center justify-between px-4 py-3">
+    <div>
+      <button type="button" className="block text-gray-100">
+        <svg
+          className="h-6 w-6 fill-current"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+        >
+          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+        </svg>
+      </button>
+    </div>
+    <div>
+      <img className="h-8" src="/" alt="Unbabel"></img>
+    </div>
+  </header>
+);
 
 export default header;
