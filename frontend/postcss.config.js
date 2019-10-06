e81870ec68707eefcc966250d14165af5298652c
amosbastian <amosbastian@gmail.com>
2019-10-06 15:24:50 +0100

    Update postcss.config.js

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,8 +1,15 @@
-//postcss.config.js
+// postcss.config.js
 const tailwindcss = require('tailwindcss');
+
 module.exports = {
-    plugins: [
-        tailwindcss('./tailwind.js'),
-        require('autoprefixer'),
-    ],
+  plugins: [
+    tailwindcss('./tailwind.js'),
+    require('autoprefixer'),
+    require('@fullhuman/postcss-purgecss')({
+      content: ['./src/**/*.jsx', './src/**/*.js', './public/index.html'],
+      css: ['./src/assets/styles/tailwind.css'],
+      // Include any special characters you're using in this regular expression
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }),
+  ],
 };
